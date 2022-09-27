7f2a9e2971c6f85040e2770deb9b29847455afa5
AnuragPacholi <anuragpacholi2000@gmail.com>
2022-09-27 18:03:23 +0530

    The Web App is Ready

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 # import sklearn
 import pickle
+from typing import final
 from flask import Flask, json,request,app,jsonify,url_for,render_template
 import numpy as np
 import pandas as pd
@@ -31,7 +32,25 @@ def predict_api(): # Making the API.
     return jsonify(output[0]) # Returning the prediction.
 
     # As you can notice in this API we are doing every major thing, we are reshaping the input, sending it, predicting from the model and getting the output.
-    # We'll also use Postman to use this API.
+    # We'll also use Postman to test this API.
 
-if __name__=="__main__": # Neede to run the app.
+
+# WITHOUT USING AN API:
+@app.route('/predict',methods=['POST'])
+def predict():
+    data = [float(x) for x in request.form.values()]
+    # Here in the above line we are capturing all the values that we got from out html form(i.e. homepage of out app).
+    # So basically we are requesting for those input values in a list. We want them in float type.
+    final_input = scalar.transform(np.array(data).reshape(1,-1)) # Standardizing and reshaping.
+    print(final_input)
+    output = regmodel.predict(final_input)[0] # Prediction
+    return render_template("home.html", prediction_text = 'The Predicted House Price is {}'.format(output))
+    # In the above line we are returning a template and in it we'll have a place holder i.e. our prediction text which also contains the output value using the '.format'.
+    # The template which we are returning above is the same as our homepage. That means we'll show the result in the same page. You can show a different page also, just make a template/html page for it.
+
+# THIS ABOVE PREDICTION FUNCTION COULD WORK WITHOUT THE API THAT WE BUILT PERHAPS.
+
+
+
+if __name__=="__main__": # Needed to run the app.
     app.run(debug=True)
